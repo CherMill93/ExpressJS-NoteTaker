05ec9f2acfc61d4f7146d5ed979657ad1d522281
CherMill <cmille2016@gmail.com>
2022-04-09 09:33:19 -0600

    route reworks

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,19 @@
-const note_data = require("../db/note_data");
+const note_data = require("../db/db.json");
 const { createNewNote, deleteNote } = require("../assets/js/index")
 const app = require("express").Router();
 
 // exporting api paths
 
-app.get("/note", (req,res) => {
+app.get('/notes', (req,res) => {
    res.json(note_data);
 });
 
-app.post("/note", (req,res) => {
+app.post('/notes', (req,res) => {
     let create = createNewNote(req.body, note_id)
    res.json(create);
 });
 
-app.delete("note/id", function(req,res) {
+app.delete('/notes/id', function(req,res) {
    deleteNote(note_data, req.params.id);
    res.json(note_data)
 })
